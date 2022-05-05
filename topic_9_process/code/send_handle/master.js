add52a6a702e886cb4e9ecbe1f378b7beb52162d
randomVariable2000 <1597888126@qq.com>
2022-05-05 16:43:07 +0800

    feat: send handle example

---
+++ topic_9_process/code/send_handle/master.js
@@ -0,0 +1,10 @@
+const child = require('child_process').fork('./child.js')
+
+const server = require('net').createServer();
+server.on('connection', function(socket) {
+	socket.end('handled by parent\n');
+})
+
+server.listen(1337, function() {
+	child.send('server', server)
+})
