952a9e376678d66535cd286946f6f0adafc9ea83
randomVariable2000 <1597888126@qq.com>
2022-05-07 09:50:15 +0800

    feat: init base server handler

---
+++ topic_8_build_web_application/code/base.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+http
+  .createServer(function (req, res) {
+    console.log(req, res);
+  })
+  .listen(3000);
+
+/**
+ * curl 'http://127.0.0.1:3000/' \
+  -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"' \
+  -H 'Referer: ' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'Content-Type: text/plain;charset=UTF-8' \
+  --data-raw '{a: 11}' \
+  --compressed
+*/
